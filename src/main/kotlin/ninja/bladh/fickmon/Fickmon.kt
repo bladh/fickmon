8d1477df008d296125f21e666f2da922fa7c7442
Alexander Bladh <bladh.alexander@gmail.com>
2017-06-05 22:32:02 +0200

    Add missing stats and evolution info

--- src/main/kotlin/ninja/bladh/fickmon/Fickmon.kt
+++ src/main/kotlin/ninja/bladh/fickmon/Fickmon.kt
@@ -8,13 +8,17 @@ data class Fickmon(
         val frontImagePath: String,
         val backImagePath: String,
         val smallImagePath: String,
+        val baseHealth: Double,
         val baseAttack: Double,
         val baseDefence: Double,
         val baseSpeed: Double,
         val baseSpecial: Double,
+        val perLevelHealth: Double,
         val perLevelAttack: Double,
         val perLevelDefence: Double,
         val perLevelSpeed: Double,
         val perLevelSpecial: Double,
-        val learnedMoves: Map<Int, Move>
+        val learnedMoves: Map<Int, Move>,
+        val evolutionLevel: Int,
+        val evolutionMon: Fickmon
 )
