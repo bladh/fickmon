fbb95e98243c929085eaaf3f0867a992b95bcd82
Alexander Bladh <bladh.alexander@gmail.com>
2017-06-08 22:41:42 +0200

    Rework the Type class

--- src/main/kotlin/ninja/bladh/fickmon/Type.kt
+++ src/main/kotlin/ninja/bladh/fickmon/Type.kt
@@ -1,9 +1,29 @@
 package ninja.bladh.fickmon
 
 data class Type(
-        val id: Int,
         val name: String,
-        val weakTo: Array<Int>,
-        val resistantTo: Array<Int>,
-        val immuneTo: Array<Int>
-)
+        val weakTo: List<String>,
+        val resistantTo: List<String>,
+        val immuneTo: List<String>
+) {
+    override fun equals(other: Any?): Boolean {
+        if (other is Type) {
+            return name == other.name
+        } else {
+            return false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode()
+    }
+
+    infix fun relationTo(type: Type): Double = relationTo(type.name)
+
+    infix fun relationTo(typeName: String): Double = when (typeName) {
+        in weakTo -> 2.0
+        in resistantTo -> 0.5
+        in immuneTo -> 0.0
+        else -> 1.0
+    }
+}
