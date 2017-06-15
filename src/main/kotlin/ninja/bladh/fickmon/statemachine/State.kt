b903d2b6f16328934d895783abcad3e694630c58
Alexander Bladh <bladh.alexander@gmail.com>
2017-06-15 23:22:28 +0200

    Make State enter() and leave() methods optional to implement
    Also optionally supply a Message when enter()ing

--- src/main/kotlin/ninja/bladh/fickmon/statemachine/State.kt
+++ src/main/kotlin/ninja/bladh/fickmon/statemachine/State.kt
@@ -1,8 +1,8 @@
 package ninja.bladh.fickmon.statemachine
 
 abstract class State(val parentState: State?) {
-    abstract fun enter()
-    abstract fun leave()
+    open fun enter(message: Message? = null) {}
+    open fun leave() {}
     /**
      * Return true if the message was handled by this state
      * Return false if not, and a potential parent state will attempt to handle it
